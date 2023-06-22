d4672de49a71ef641a61fe6f933c5f21a26056e8
Leonid Karlovskii <karlovskiyleo@gmail.com>
2023-06-22 09:42:35 +0300

    continue user refactor

---
+++ src/main/java/com/example/picturegramm/PictureGrammApplication.java
@@ -0,0 +1,14 @@
+package com.example.picturegramm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureGrammApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureGrammApplication.class, args);
+
+    }
+
+}
---
+++ src/main/java/com/example/picturegramm/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.example.picturegramm.dto;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UserDto {
+
+
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String created;
+
+}
