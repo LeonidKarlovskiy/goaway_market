df2cf588a1808db4d4c2b71fb12e3fc7f0e13913
Leonid Karlovskii <karlovskiyleo@gmail.com>
2023-06-22 09:41:12 +0300

    continue user refactor

---
+++ src/main/java/com/example/picturegramm/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.picturegramm.repository;
+
+import com.example.picturegramm.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findUserById(Long id);
+
+    Optional<User> findUserByName(String name);
+
+
+
+}
